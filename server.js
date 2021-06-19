3440be8214890640786c17ee642c2203447044cf
David Sackettgit config --global user.name David <davidsackett14@gmail.com>
2021-06-18 18:01:24 -0600

    deploying again

--- server.js
+++ server.js
@@ -10,15 +10,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
 
 // routes
 app.use(require("./routes/api.js"));
